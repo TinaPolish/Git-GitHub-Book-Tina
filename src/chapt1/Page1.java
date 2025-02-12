bb92596163a08ca3038b0c8b9a233ac0845f8c13
ValentinaPolishchuk <tinapolishchuk21@gmail.com>
2025-02-12 13:35:51 -0500

    my changes test2

--- src/chapt1/Page1.java
+++ src/chapt1/Page1.java
@@ -2,6 +2,9 @@ package chapt1;
 
 public class Page1 {
     public static void main(String[] args) {
-        System.out.printf("Tina added this line");
+        
+        System.out.println("Tina added this line");
+        System.out.println("Tina adde one more line");
+        
     }
 }
